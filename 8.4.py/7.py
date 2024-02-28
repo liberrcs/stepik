f06b2695c72447622bc0b9fd67e671715fbd7eaf
Eldar <m9166699402@gmail.com>
2024-02-28 20:00:56 +0000

    stepik

---
+++ 8.4.py/7.py
@@ -0,0 +1,6 @@
+n,n1=input(), input()
+n2=set(n1+n)
+if len(n2) == 10:
+    print("YES")
+else:
+    print("NO")
---
+++ 8.4.py/8.py
@@ -0,0 +1,6 @@
+n=input()
+n1=input()
+if set(n1) == set(n):
+    print("YES")
+else:
+    print("NO")
