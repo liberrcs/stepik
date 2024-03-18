f96923bdb7b3dd60de39d4dcf40d2255c623f0b4
Eldar <m9166699402@gmail.com>
2024-03-18 21:50:16 +0000

    stepik 9.2

---
+++ 9.2.py/6.py
@@ -0,0 +1,6 @@
+n=input()
+n1=input()
+if set(n) == set(n1):
+    print("YES")
+else:
+    print("NO")
---
+++ 9.2.py/7.py
@@ -0,0 +1,6 @@
+m=int(input())
+n=int(input())
+math={input() for _ in range(m)}
+inf={input() for _ in range(n)}
+print(len(math-inf))
+
