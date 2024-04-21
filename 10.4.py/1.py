d3d11398d2bda85529216c808b16fc87817c1650
Eldar <m9166699402@gmail.com>
2024-04-21 15:22:38 +0000

    first commit

--- 10.4.py/1.py
+++
@@ -1,4 +0,0 @@
-data =  [(input().split(':')) for i in range(int(input()))]
-data = {i[0].lower(): i[1].strip() for i in data}
-for i in [input().lower() for i in range(int(input()))]:
-    print(data.get(i, "Не найдено"))
