a8d0c60a434b2e4609609cc4c837e459561da5b5
Eldar <m9166699402@gmail.com>
2024-05-27 01:26:32 +0300

    stepik branch 1

--- 12.2/1.py
+++ 12.2/1.py
@@ -1,7 +1,15 @@
 import random
 def generate_ip():
+    """_summary_
+An IP address consists of four numbers ranging from 
+0
+0 to 
+255
+255 (inclusive) separated by a dot.
+
+Write a function generate_ip() that, using the random module, generates and returns a random valid IP address.
+    Returns:
+        _type_: _description_
+    """
     return f'{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}'
-
-
-
-
+help(generate_ip)
