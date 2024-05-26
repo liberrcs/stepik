ef1fa3e9634a776282cabb12ba02bece38148ba9
Eldar <m9166699402@gmail.com>
2024-05-27 01:22:58 +0300

    stepik brach

--- 12.2/2.py
+++ 12.2/2.py
@@ -2,7 +2,11 @@ import random as r
 import string
 
 def generate_index():
-    """Write a function generate_index() that, using the random module, generates and returns a random valid postal code for Latveria."""
+    """_summary_
+Write a function generate_index() that, using the random module, generates and returns a random valid postal code for Latveria.
+    Returns:
+        _type_: _description_
+    """
     S = string.ascii_uppercase
     ind = (''.join(r.sample(S, 2)) + str(r.randrange(100))
     + "_" + str(r.randrange(100)) + ''.join(r.sample(S, 2)))
